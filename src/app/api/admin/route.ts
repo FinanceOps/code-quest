b94ae9367c06e636ae23370e5d2ef4443e7b1f0a
Anshul <anshul@financeops.co>
2025-01-23 17:02:32 +0530

    Update admin POST route to use a more secure secret key for authorization

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(request: NextRequest) {
   try {
     const secret = request.headers.get('x-quill-secret')
-    if (secret === 'nimda') {
+    if (secret === 'hLW*RuCP%CP@vWh*58HNM5HCx@jPKGN7QysTb#BF&') {
       return NextResponse.json({ message: 'Cleared!' }, { status: 200 })
     } else {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
